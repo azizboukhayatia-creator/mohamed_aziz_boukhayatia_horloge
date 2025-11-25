05753d6127fb19fc68eae9b581740084542570d1
azizboukhayatia-creator <azizboukhayatia@gmail.com>
2025-11-25 15:30:22 +0100

    3eme commit

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const digitalEl = document.getElementById("digital");
+const hourHand = document.getElementById("hour-hand");
+const minHand = document.getElementById("min-hand");
+const secHand = document.getElementById("sec-hand");
 function updateDigital() {
   const now = new Date();
   const h = String(now.getHours()).padStart(2, "0");
@@ -6,6 +9,23 @@ function updateDigital() {
   const s = String(now.getSeconds()).padStart(2, "0");
   digitalEl.textContent = `${h}:${m}:${s}`;
 }
+function updateAnalogClock() {
+  const now = new Date();
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
 
-setInterval(updateDigital, 200);
-updateDigital();
+  const secDeg = s * 6;
+  const minDeg = m * 6 + s * 0.1;
+  const hourDeg = (h % 12) * 30 + m * 0.5;
+
+  secHand.style.transform = `translateX(-50%) rotate(${secDeg}deg)`;
+  minHand.style.transform = `translateX(-50%) rotate(${minDeg}deg)`;
+  hourHand.style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
+}
+function updateClock() {
+  updateDigital();
+  updateAnalogClock();
+}
+setInterval(updateClock, 200);
+updateClock();
